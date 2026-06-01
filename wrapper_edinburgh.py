557a24add03fd8431c3c7b1eba756018c401ad07
a-martin-iribar <165741002+a-martin-iribar@users.noreply.github.com>
2026-06-01 07:56:33 +0100

    Add files via upload

---
+++ wrapper_edinburgh.py
@@ -0,0 +1,32 @@
+import subprocess
+
+username = input("Edinburgh username: ").strip()
+poly = input("Irreducible polynomial in x such that its splitting field has a cyclic group of order 4 as Galois group(e.g. x^4+x^3+x^2+x+1) : ").strip()
+
+remote = f"""
+set -e
+
+if [ ! -d Unit_Group_Galois_Modules ]; then
+    git clone https://github.com/ruthraistrick/Unit_Group_Galois_Modules
+fi
+
+cd Unit_Group_Galois_Modules
+
+magma <<EOF
+R<x> := PolynomialRing(Rationals());
+
+K<a> := NumberField({poly});
+
+load "Better_C4_extensions.magma";
+
+C4UnitStructure(K);
+
+quit;
+EOF
+"""
+
+subprocess.run(
+    ["ssh", f"{username}@compute64y.maths.ed.ac.uk"],
+    input=remote,
+    text=True
+)
